1d98878b96196c91b69112d2e25198adfa4602d8
jamiladaraghmeh123 <daraghmehjamila19@gmail.com>
2026-06-12 10:44:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import json
+from soil_analyzer import SoilAnalyzer
+from visualizer import Visualizer
 
+
+def main():
+    DATA_FILE = "soil_readings.csv"
+    REPORT_FILE = "soil_report.json"
+
+    analyzer = SoilAnalyzer()
+    viz = Visualizer()
